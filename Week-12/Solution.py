bb46723413f406c6374a2b1df628cf19a1280e3b
nfeingold-cerium <nfeingold@ceriumnetworks.com>
2022-01-28 13:25:50 -0800

    Fixed 11 and 12 order

--- Week-12/Solution.py
+++ Week-12/Solution.py
@@ -1,15 +1,17 @@
 from getpass import getpass
 import netmiko
 
-IP       : str = "10.10.221.176"
-HOSTNAME : str = "Randy Test Box"
+IP       : str = "10.12.222.113"
+HOSTNAME : str = "Test Devices"
 DEVICE_TYPE : str = "cisco_nxos"
 
-USERNAME : str = "dadmin"
-PASSWORD : str = "dadmin01"
+USERNAME : str = "admin"
+PASSWORD : str = ""
 PORT : str = ""
 
 def main():
+
+    # Create Initial Connection Template
     sshTemplate = {
         'username' : USERNAME or input("Please Enter Username: "),
         'password' : PASSWORD or getpass("Please enter your password: "),
@@ -17,12 +19,51 @@ def main():
         'ip' : IP or input("Please enter device IP: "),
     }
 
+    # Connect to Inital Device
     conn = netmiko.Netmiko(**sshTemplate)
-    res = conn.send_command('testinads')
+    
+    # Send Command to Initial Device
+    res = conn.send_command('show cdp neighbors detail', use_textfsm=True)
 
-    print(res)
+    # Set Up Sets
+    devicesConnectedTo = set([sshTemplate['ip']])
+    devicesToConnectTo = set()
+
+    # Populate original devices to connect to
+    for device in res:
+        ip = device['interface_ip']
+        devicesToConnectTo.add(ip)
+
+    # Connect to devices in set while we can
+    while(devicesToConnectTo):
+
+        # Note that we connected to new device
+        devicesConnectedTo.add(ip)
+
+        # Update SSH Template to connect to new device
+        ip = devicesToConnectTo.pop()
+        sshTemplate['ip'] = ip
+
+        # Make sure we haven't connect already
+        if ip not in devicesConnectedTo:
+            try:
+                # Connect to new device and get neighbors
+                conn = netmiko.Netmiko(**sshTemplate)
+                newRes = conn.send_command('show cdp neighbors detail', use_textfsm=True)
+
+                # Add neighbors to devices to connect to
+                for device in newRes:
+                    ip = device['interface_ip']
+                    devicesToConnectTo.add(ip)
+
+            except Exception as e:
+                print(f"Failed to connect to {ip}")
+
+
+    print(devicesConnectedTo)
 
 
 
 if __name__ == '__main__':
     main()
+
